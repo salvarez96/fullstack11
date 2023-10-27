8087816d4fc494946d58e7eb69c7442160ad7d2e
Santiago Bernal <exactlimon@gmail.com>
2023-10-26 19:23:16 -0500

    feat: array methods

--- es6.js
+++ es6.js
@@ -22,5 +22,47 @@ const inicio = () => {
   console.log(result);
 }
 
-inicio();
+//inicio();
+
+
+// IIFE
+// inmediately invoked function expression
+(function () {
+  console.log("hola mundo");
+})();
+
+// arrays
+
+const baloto = [18, 28, 38, 80, 90];
+console.log(baloto);
+
+// agregar un elemento al final del array
+baloto.push(30);
+console.log(baloto);
+
+// agregar un elemento al inicio del array
+baloto.unshift(4);
+console.log(baloto);
+
+// eliminar el ultimo elemento del array
+baloto.pop();
+console.log(baloto);
+
+// eliminar el primer elemento del array
+baloto.shift();
+console.log(baloto);
+
+// map, con map podemos retornar el valor en una variable
+const array2 = baloto.map((item) => item = item * 2);
+
+console.log(array2);
+
+// foreach, con foreach no podemos retornar el valor en una variable
+baloto.forEach((el) => {
+  console.log(el);
+});
+
+// filter, con filter podemos filtrar los elementos de un array
+const filteredNumbers = baloto.filter((el) => el >= 30);
+console.log(filteredNumbers);
 
