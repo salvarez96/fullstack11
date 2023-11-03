18cede14e350792f4f8af93ec92ae3dc61801881
Santiago Bernal <exactlimon@gmail.com>
2023-11-02 19:00:52 -0500

    feat: OOP Encapsulation

--- poo.js
+++ poo.js
@@ -1,14 +1,28 @@
 class Persona {
   // atributos
-  nombre;
-  edad;
+  #nombre;
+  #edad;
   // el constructor es el que permite crear objetos de una clase
   constructor(nombre, LaEdad) {
-    this.nombre = nombre;
-    this.edad = LaEdad;
+    this.#nombre = nombre;
+    this.#edad = LaEdad;
   }
   saludar() {
-    console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años`);
+    console.log(`Hola, mi nombre es ${this.#nombre} y tengo ${this.#edad} años`);
+  }
+  //encapsulamiento
+  // getters y setters permiten acceder a los atributos privados,
+  getNombre() {
+    return this.#nombre;
+  }
+  setNombre(nuevoNombre) {
+    this.#nombre = nuevoNombre;
+  }
+  getEdad() {
+    return this.#edad;
+  }
+  setEdad(nuevaEdad) {
+    this.#edad = nuevaEdad;
   }
 }
 
@@ -17,4 +31,6 @@ const persona1 = new Persona('Santiago', 19);
 const persona2 = new Persona('Juan', 30);
 
 persona1.saludar();
-console.log(persona1.nombre);
+console.log(persona1.getNombre());
+persona1.setEdad(20);
+persona1.saludar();
