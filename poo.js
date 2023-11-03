2f46724df56dbc64ddfc60803f57c0450f23f540
Santiago Bernal <exactlimon@gmail.com>
2023-11-02 19:17:37 -0500

    feat: OOP Inheritance

--- poo.js
+++ poo.js
@@ -26,6 +26,65 @@ class Persona {
   }
 }
 
+class Empleado extends Persona {
+  #sueldo;
+  constructor(nombre, edad, sueldo) {
+    super(nombre, edad);
+    this.#sueldo = sueldo;
+  }
+  saludar() {
+    console.log(`Hola, mi nombre es ${this.getNombre()} y tengo ${this.getEdad()} años y mi sueldo es ${this.#sueldo} dolares`);
+  }
+}
+
+class Cliente extends Persona {
+  #numeroCuenta;
+  #SaldoCuenta;
+  constructor(nombre, edad, numeroCuenta, SaldoCuenta) {
+    super(nombre, edad);
+    this.#numeroCuenta = numeroCuenta;
+    this.#SaldoCuenta = SaldoCuenta;
+  }
+  saludar() {
+    console.log(`Hola, mi nombre es ${this.getNombre()} y tengo ${this.getEdad()} años y mi numero de cuenta es ${this.#numeroCuenta} `);
+  }
+  depositar(monto) {
+    this.#SaldoCuenta += monto;
+  }
+  retirar(monto) {
+    this.#SaldoCuenta -= monto;
+  }
+  getSaldo() {
+    return this.#SaldoCuenta;
+  }
+  SetSaldo(nuevoSaldo) {
+    this.#SaldoCuenta = nuevoSaldo;
+  }
+
+}
+
+class Sucursal {
+  #numeroSucursal;
+  #direccionSucursal;
+  #empleados;
+  constructor(numeroSucursal, direccionSucursal) {
+    this.#numeroSucursal = numeroSucursal;
+    this.#direccionSucursal = direccionSucursal;
+    this.#empleados = [];
+  }
+  agregarEmpleado(empleado) {
+    this.#empleados.push(empleado);
+  }
+  eliminarEmpleado(empleado) {
+    const indice = this.#empleados.indexOf(empleado);
+    this.#empleados.splice(indice, 1);
+  }
+  getEmpleados() {
+    return this.#empleados;
+  }
+
+}
+
 // aqui estoy creando un objeto de la clase Persona
 const persona1 = new Persona('Santiago', 19);
 const persona2 = new Persona('Juan', 30);
@@ -34,3 +93,13 @@ persona1.saludar();
 console.log(persona1.getNombre());
 persona1.setEdad(20);
 persona1.saludar();
+
+const gerente = new Empleado('Pedro', 40, 5000);
+gerente.saludar();
+
+const clienteDelMes = new Cliente('Maria', 25, 123456, 0);
+clienteDelMes.saludar();
+clienteDelMes.depositar(1000);
+console.log(clienteDelMes.getSaldo());
+clienteDelMes.retirar(600);
+console.log(clienteDelMes.getSaldo());
