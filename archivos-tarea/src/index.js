4b1a773e4b54c8bf41d1d5f17f57f41183686e3c
salvarez96 <santiagoalvarez9617@gmail.com>
2023-12-02 15:33:43 -0500

    Add product creation logic
    Add price input for shopping list

--- archivos-tarea/src/index.js
+++ archivos-tarea/src/index.js
@@ -1,1 +1,40 @@
+function $(domElement) {
+  return document.querySelector(domElement)
+}
+
+function $$(domElements) {
+  return document.querySelectorAll(domElements)
+}
+
+function elementCreator(domElement, elementClass) {
+  const newElement = document.createElement(domElement)
+  newElement.classList.add(elementClass)
+  return newElement
+}
+
+const submitButton = $('button')
+const productInput = $('#productItem')
+const priceInput = $('#productPrice')
+const itemsList = $('#itemsList')
+
+let productsList = []
+let productId = 0
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  if(productInput.value.length > 0) {
+    productsList.push({
+      id: productId++,
+      product: productInput.value,
+      price: priceInput.value
+    })
+    // const productItemElement = elementCreator('li')
+    // productItemElement.innerText = productsList[productsList.length - 1]
+    // itemsList.insertAdjacentElement('beforeend', productItemElement)
+    
+  }
+  console.log(productsList);
+})
+
 console.log('object');
